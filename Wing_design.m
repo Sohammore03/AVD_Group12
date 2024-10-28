1d9755d064684539067bffba7697fb31cd2dcce3
Erica437360 <erica.keung22@imperial.ac.uk>
2024-10-28 15:58:29 +0000

    first version

---
+++ Wing_design.m
@@ -0,0 +1,11 @@
+clear
+clc
+clf
+close all
+
+%--------------------------------------------------------------------------
+%Finding the Reynold's number for each mission segment
+%Cruise 1 
+rho_c1 = 0.380305; %Density of air at cruise (kg/m3)
+u_c1 = 245.14824; %Speed of air at cruise 1 (m/s-1)
+L = 
